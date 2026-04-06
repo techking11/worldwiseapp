5780b826479b2bf18223d43a1c3651517ef73a55
techking11 <nma1132000@gmail.com>
2026-04-06 06:44:31 +0400

    fix: wrong act import

--- src/context/FakeAuthContext.jsx
+++ src/context/FakeAuthContext.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { act, createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
